6142f3b924adecc538bbcb89fc4ceb9b7909f2a9
Pierre Grimaud <pierre@shopping-flux.com>
2016-04-03 01:56:16 +0200

    test

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
 ini_set('display_errors', 0);
 
 $images = [
-    'default'   => 'i2600',
-    'metro1'    => 'i2605',
-    'metro2'    => 'i2606',
-    'metro3'    => 'i2608',
-    'metro3b'   => 'i2607',
-    'metro4'    => 'i2609',
-    'metro5'    => 'i2610',
-    'metro6'    => 'i2590',
-    'metro7'    => 'i2611',
-    'metro7b'   => 'i2612',
-    'metro8'    => 'i2613',
-    'metro9'    => 'i2614',
-    'metro10'   => 'i2615',
-    'metro11'   => 'i2617',
-    'metro12'   => 'i2618',
-    'metro13'   => 'i2616',
-    'metro14'   => 'i2619',
-    'rera'      => 'i2620',
-    'rerb'      => 'i2621'
+    'default' => 'i2600',
+    'metro1' => 'i2605',
+    'metro2' => 'i2606',
+    'metro3' => 'i2608',
+    'metro3b' => 'i2607',
+    'metro4' => 'i2609',
+    'metro5' => 'i2610',
+    'metro6' => 'i2590',
+    'metro7' => 'i2611',
+    'metro7b' => 'i2612',
+    'metro8' => 'i2613',
+    'metro9' => 'i2614',
+    'metro10' => 'i2615',
+    'metro11' => 'i2617',
+    'metro12' => 'i2618',
+    'metro13' => 'i2616',
+    'metro14' => 'i2619',
+    'rera' => 'i2620',
+    'rerb' => 'i2621'
 ];
 
 $transportTypes = [
@@ -30,21 +30,21 @@ $transportTypes = [
     'tramway'
 ];
 
-$line           = $_GET['line'];
-$destination    = $_GET['destination'];
-$station        = $_GET['station'];
+$line = $_GET['line'];
+$destination = $_GET['destination'];
+$station = $_GET['station'];
 
 $icon = isset($images[$line]) ? $images[$line] : $images['default'];
 
-foreach($transportTypes as $transportType){
-    if(strpos($line, $transportType) !== false){
+foreach ($transportTypes as $transportType) {
+    if (strpos($line, $transportType) !== false) {
         $type = $transportType;
         $line = str_replace($transportType, '', $line);
         break;
     }
 }
 
-$urlToCall = 'http://api-ratp.pierre-grimaud.fr/v2/'.$type.'s/'.$line.'/stations/'.$station.'?destination='.$destination;
+$urlToCall = 'http://api-ratp.pierre-grimaud.fr/v2/' . $type . 's/' . $line . '/stations/' . $station . '?destination=' . $destination;
 
 $file = file_get_contents($urlToCall);
 $json = json_decode($file);
@@ -58,13 +58,13 @@ $data = [
     'frames' => [
         [
             'index' => 0,
-            'text'  => 'Prochain train pour '.$responseDestination,
-            'icon'  => $icon
+            'text' => $responseDestination,
+            'icon' => $icon
         ],
         [
             'index' => 1,
-            'text'  => $responseSchedule,
-            'icon'  => $icon
+            'text' => str_replace('MN', 'MIN', $responseSchedule),
+            'icon' => $icon
         ]
     ]
 ];
